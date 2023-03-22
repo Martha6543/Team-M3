c6845e6cbdf30c9662c512259f182654efde5370
marwayu <118738622+marwayu@users.noreply.github.com>
2023-03-22 18:45:12 +0000

    Update Header.js

--- Header.js
+++ Header.js
@@ -76,25 +76,3 @@ export default Header;
 
 
 
-/*
-<div className="input-container">
-<input type="text" name="text" className="input" placeholder="search..."/>
-<span className="icon"> 
-  <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path opacity="1" d="M14 5H20" stroke="#000" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path>
-    <path opacity="1" d="M14 8H17" stroke="#000" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path>
-    <path d="M21 11.5C21 16.75 16.75 21 11.5 21C6.25 21 2 16.75 2 11.5C2 6.25 6.25 2 11.5 2" stroke="#000" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round"></path>
-    <path opacity="1" d="M22 22L20 20" stroke="#000" strokeWidth="3.5" strokeLinecap="round" strokeLinejoin="round"></path>
-  </svg>
-</span>
-</div>
-
-
-
-<div className="navbar-dropdown-content">
-<a href="/running" className="dropdown-item">Running</a>
-<a href="/climbing" className="dropdown-item">Climbing</a>
-<a href="/football" className="dropdown-item">Football</a>
-<a href="/tennis" className="dropdown-item">Tennis</a>
-<a href="/hiking" className="dropdown-item">Hiking</a>
-</div> */
